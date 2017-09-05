e30b913b011a6f60c444aafdb39a3e83dd730ab3
Muzafar Umarov <mumarov@optoro.com>
2017-09-04 22:52:55 -0400

    Add server part with post and get all

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+  name: {
+    type: 'String',
+    required: true,
+    trim: true,
+    minlength: 1
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+module.exports = {
+  User
+};
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+const { Todo } = require('./models/todo');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/todos', async (req, res) => {
+  res.send(await Todo.find({}))
+});
+
+app.post('/todos', async (req, res) => {
+  const todo = new Todo(req.body)
+  try {
+    await todo.save();
+    res.send(todo);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.listen(port, () => console.log(`Started on port ${port}`));
